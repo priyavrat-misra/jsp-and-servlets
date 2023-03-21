4773f7c505a0123c2ad27d5e8c8a11233efd0088
Priyavrat Misra <priyavratmisra123@gmail.com>
2023-03-21 16:11:10 +0530

    add examples for doGet and doPost

---
+++ HelloWorld/WEB-INF/classes/com/example/servlets/HelloServlet.java
@@ -0,0 +1,47 @@
+package com.example.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// http://localhost:8080/HelloWorld/hello?username=World&symbol=?
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter writer = response.getWriter();
+		response.setContentType("text/html");
+		String username = request.getParameter("username");
+		String symbol = request.getParameter("symbol");
+		writer.println("<!DOCTYPE html><html><head>");
+		writer.println("<title>Hello!</title></head><body>Hello ");
+		if (username == null && symbol == null)
+			writer.println("World!");
+		else if (username == null)
+			writer.println("World" + symbol);
+		else if (symbol == null)
+			writer.println(username + "!");
+		else
+			writer.println(username + symbol);
+		writer.println("</body></html>");
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter writer = response.getWriter();
+		response.setContentType("text/html");
+		String email = request.getParameter("mail");
+		String password = request.getParameter("pass");
+		writer.println("<!DOCTYPE html><html><head>");
+		writer.println("<title>Hello!</title></head><body>");
+		if (email != null && password != null)
+			writer.println("Logged in successfully!");
+		else
+			writer.println("Something went wrong.");
+		writer.println("</body></html>");
+	}
+
+}
+
